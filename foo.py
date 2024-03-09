3c8af3a648e9b93d6b7e3526051fa075b3baa3fe
Danis Akhmadiev <iamdanisakhmadiev>
2024-03-09 15:39:27 +0400

    added code in main.py

---
+++ foo.py
@@ -0,0 +1,4 @@
+my_dict = {'a': 1, 'b': 2}
+
+for i in my_dict:
+    print(i)
---
+++ main.py
@@ -0,0 +1,63 @@
+import json
+from datetime import datetime
+with open('operations.json', 'r') as file:
+    data = json.load(file)
+
+
+def executed_operations(operations):
+    """Возвращает переменную в которой содержаться только выполненные операции"""
+    executed_operations_list = []
+    for operation in operations:
+        if "state" in operation:
+            if operation["state"] == "EXECUTED":
+                executed_operations_list.append(operation)
+    return executed_operations_list
+
+
+def convert_date(operations):
+    """берет дату операций и со str конвертирует в datetime"""
+    convert_operations = []
+    for operation in operations:
+        operation_time = datetime.strptime(operation["date"], "%Y-%m-%dT%H:%M:%S.%f")
+        formatted_operation_time = operation_time.strftime("%d.%m.%Y")
+        operation["date"] = formatted_operation_time
+        convert_operations.append(operation)
+    return convert_operations
+
+
+def print_last_5_operations(operations):
+    """Сортируем операции по дате в обратном порядке, а потом возвращает последние 5 операций"""
+    sorted_operations = sorted(operations, key=lambda x: x['date'], reverse=True)
+    return sorted_operations[:5]
+
+
+def mask_card_number(card):
+    """берет значение по ключу from и оставляет только номер карты, потом маскирует номер """
+    digits_only = ''.join(c for c in card if c.isdigit())
+    card_name = ''.join(c for c in card if not c.isdigit())
+    if card_name[-1] == ' ':
+        card_name = card_name[:-1]
+    if card_name != 'Счет':
+        blocks = [digits_only[i:i+4] for i in range(0, len(digits_only), 4)]
+        masked_number = blocks[0] + ' ' + blocks[1][:2] + '**' + ' ' + '****' + ' ' + blocks[-1]
+    else:
+        masked_number = "**" + digits_only[2:6]
+    return masked_number
+
+
+def get_card_name(card):
+    """берет значение по ключу from и to и возвращает метод платежа"""
+    card_name = ''.join(c for c in card if not c.isdigit())
+    if card_name[-1] == ' ':
+        card_name = card_name[:-1]
+    return card_name
+
+
+d1 = executed_operations(data)
+d1 = convert_date(d1)
+d1 = print_last_5_operations(d1)
+for i in range(len(d1)):
+    print(d1[i]['date'], d1[i]['description'])
+    print(get_card_name(d1[i]['from']), mask_card_number(d1[i]['from']), '->', get_card_name(d1[i]['to']),
+          mask_card_number(d1[i]['to']))
+    print(d1[i]['operationAmount']['amount'], d1[i]['operationAmount']['currency']['name'], '\n')
